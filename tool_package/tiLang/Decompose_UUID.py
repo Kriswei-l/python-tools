8588039ed7c863586317f8136874f5a5cf04189d
Kriswei-l <72719622+Kriswei-l@users.noreply.github.com>
2021-04-14 16:19:55 +0800

    新写了一个通过_children替换UUID

--- tool_package/tiLang/Decompose_UUID.py
+++ tool_package/tiLang/Decompose_UUID.py
@@ -74,9 +74,73 @@ def find_ti_uuid(path, fileType):
             uuid = start_find_uuid(path+'.'+d+'.meta', fileName)
             # print(uuid)
             return uuid
+    
+    return
+# -------------------------------------------------------------------
+# 解析component
+def analysis_component(data, typeTab, component, reuuid, langPath, fileType, path):
+    if component is None:
+        return
+    if component['__type__'] == reuuid:
+        # print(data[typeTab]['_spriteFrame']['__uuid__'])
+        # 替换UUID
+        if component['i18n_string']:
+            # print(langPath+'/'+d['i18n_string'])
+            tiUUID = find_ti_uuid(langPath+'/'+component['i18n_string'], fileType)
+            if tiUUID:
+                if data[typeTab]['_spriteFrame']:
+                    data[typeTab]['_spriteFrame']['__uuid__'] = tiUUID
+                else:
+                    print(path)
+            else:
+                #找不到替换的图片时，把现在语言的uuid设置为空
+                if data[typeTab]['_spriteFrame']:
+                    data[typeTab]['_spriteFrame']['__uuid__'] = ""
 
-# 打开文件查找reuuid
-def open_prefab_find(path, reuuid, langPath, fileType, typeName):
+# 查找节点的component
+def find_components_type(data, dComponent, reuuid, langPath, fileType, typeName, path):
+    if data is None:
+        return
+    if dComponent is None:
+        return
+    if len(dComponent) <= 0:
+        return
+    # 判断component是否有'cc.Sprite'和长度是否大于1
+    if len(dComponent) < 2:
+        return
+    typeTab = 0
+    for j in range(0, len(dComponent)):
+        dIndex = dComponent[j]['__id__']
+        dChild_d = data[dIndex]
+        if dChild_d['__type__'] == typeName:
+            typeTab = dIndex
+    for j in range(0, len(dComponent)):
+        dIndex = dComponent[j]['__id__']
+        dChild_d = data[dIndex]
+        if dChild_d['__type__'] == reuuid:
+            analysis_component(data, typeTab, dChild_d, reuuid, langPath, fileType, path)
+
+
+# 查找节点
+def find_child_type(data, dsite, reuuid, langPath, fileType, typeName, path):
+    if data is None:
+        return
+    if dsite is None:
+        return
+    # 查找Components
+    find_components_type(data, dsite['_components'], reuuid, langPath, fileType, typeName, path)
+
+    # 查看子节点
+    dChild = dsite['_children']
+    if len(dChild) <= 0:
+        return
+    for j in range(0, len(dChild)):
+        dChild_d = dChild[j]
+        # print(dChild_d['__id__'])
+        # print(data[dChild_d['__id__']])
+        find_child_type(data, data[dChild_d['__id__']], reuuid, langPath, fileType, typeName, path)
+
+def open_prefab(path, reuuid, langPath, fileType, typeName):
     data = ""
     contentData = ""
     bLine = 0
@@ -89,42 +153,28 @@ def open_prefab_find(path, reuuid, langPath, fileType, typeName):
             contentData += (contentStr)
             bLine += 1
     data = json.loads(contentData)  #prefab内容
+    # 判断数据的类型
     # print(type(data))
     # print(isinstance(data, lsit))
     # print(data)
     
-    typeTab = 0
-    typeindx = 0
     for i in range(0,len(data)):
         d = data[i]
         # print(d)
-        if d['__type__'] == typeName:
-            typeindx += 1
-            typeTab = i
-        if typeindx == 1 and d['__type__'] == reuuid:
-            typeindx -= 1
-            # print(data[typeTab]['_spriteFrame']['__uuid__'])
-            # data[typeTab]['_spriteFrame']['__uuid__'] = "1"
-            # 替换UUID
-            if d['i18n_string']:
-                # print(langPath+'/'+d['i18n_string'])
-                tiUUID = find_ti_uuid(langPath+'/'+d['i18n_string'], fileType)
-                if tiUUID:
-                    if data[typeTab]['_spriteFrame']:
-                        data[typeTab]['_spriteFrame']['__uuid__'] = tiUUID
-                    else:
-                        print(path)
-                    typeTab = 0
-                else:
-                    #找不到替换的图片时，把现在语言的uuid设置为空
-                    if data[typeTab]['_spriteFrame']:
-                        data[typeTab]['_spriteFrame']['__uuid__'] = ""
-            typeTab = 0
-        if d['__type__'] == 'cc.PrefabInfo' or d['__type__'] == 'cc.Node':
-            typeindx = 0
-            typeTab = 0
-        if d['__type__'] == typeName and typeindx > 1:
-            typeindx -= 1
+        if d['__type__'] == 'cc.Node':
+            # print(d['_children'])
+            dChild = d['_children']
+            # print(len(dChild))
+            for j in range(0, len(dChild)):
+                dChild_d = dChild[j]
+                # print(dChild_d['__id__'])
+                # print(data[dChild_d['__id__']])
+                find_child_type(data, data[dChild_d['__id__']], reuuid, langPath, fileType, typeName, path)
+            
+            # 查找第一层节点的component
+            find_components_type(data, d['_components'], reuuid, langPath, fileType, typeName, path)
+
+            break
     
 
     # 数据写入文件
@@ -135,6 +185,7 @@ def open_prefab_find(path, reuuid, langPath, fileType, typeName):
         else:
             dJson = listToJsonEx(data)
             fw.write(dJson)
+
 
 # 查找所有的prefab并替换
 def find_prefab(reuuid, fileType, typeName):
@@ -160,7 +211,7 @@ def find_prefab(reuuid, fileType, typeName):
                 scene = alist[1].strip()
     
     # 查找单独的一个prefab
-    # open_prefab_find("./tool_package/Login.fire", reuuid, language, fileType, typeName)
+    # open_prefab("./tool_package/Login.fire", reuuid, language, fileType, typeName)
     # return
     # 
     for root, dirs, files in os.walk(pathPrefab):
@@ -172,7 +223,7 @@ def find_prefab(reuuid, fileType, typeName):
         for f in files:
             path=os.path.join(root, f)
             if path.find('.prefab') != -1 and path.find('.meta') == -1:
-                open_prefab_find(path, reuuid, language, fileType, typeName)
+                open_prefab(path, reuuid, language, fileType, typeName)
     for root, dirs, files in os.walk(pathtexture):
         # root 表示当前正在访问的文件夹路径
         # dirs 表示该文件夹下的子目录名list
@@ -182,7 +233,7 @@ def find_prefab(reuuid, fileType, typeName):
         for f in files:
             path=os.path.join(root, f)
             if path.find('.prefab') != -1 and path.find('.meta') == -1:
-                open_prefab_find(path, reuuid, language, fileType, typeName)
+                open_prefab(path, reuuid, language, fileType, typeName)
     # 查找场景
     for root, dirs, files in os.walk(scene):
         # root 表示当前正在访问的文件夹路径
@@ -193,65 +244,10 @@ def find_prefab(reuuid, fileType, typeName):
         for f in files:
             path=os.path.join(root, f)
             if path.find('.fire') != -1 and path.find('.meta') == -1:
-                open_prefab_find(path, reuuid, language, fileType, typeName)
+                open_prefab(path, reuuid, language, fileType, typeName)
 
-def find_child_type(data, site):
-    if data is None:
-        return
-    d = data[site]
-    if d is None:
-        return
-    dChild = d['_children']
-    for j in range(0, len(dChild)):
-        dChild_d = dChild[j]
-        # print(dChild_d['__id__'])
-        # print(data[dChild_d['__id__']])
-        find_child_type(data, data[dChild_d['__id__']])
-
-def open_prefab(path):
-    data = ""
-    contentData = ""
-    bLine = 0
-    with open(path, 'rb') as infile:
-        while True:
-            content = infile.readline()
-            if not content:
-                break
-            contentStr=str(content,encoding='utf-8')
-            contentData += (contentStr)
-            bLine += 1
-    data = json.loads(contentData)  #prefab内容
-    # 判断数据的类型
-    # print(type(data))
-    # print(isinstance(data, lsit))
-    # print(data)
-    
-    for i in range(0,len(data)):
-        d = data[i]
-        # print(d)
-        if i == 1 and d['__type__'] == 'cc.Node':
-            # print(d['_children'])
-            dChild = d['_children']
-            # print(len(dChild))
-            for j in range(0, len(dChild)):
-                dChild_d = dChild[j]
-                # print(dChild_d['__id__'])
-                # print(data[dChild_d['__id__']])
-                find_child_type(data, data[dChild_d['__id__']])
-
-            break
-    
-
-    # 数据写入文件
-    with open(path,"w+") as fw:
-        if bLine > 1:
-            dJson = listToJson(data)
-            fw.write(dJson)
-        else:
-            dJson = listToJsonEx(data)
-            fw.write(dJson)
 if __name__ == '__main__':
-    open_prefab('./tool_package/createAccount.prefab')
+    # open_prefab('./tool_package/createAccount.prefab')
     # # src_dir = r'D:\FishingGameClient\build\jsb-default\res\raw-assets'
     # nw_path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
     # print(nw_path)
@@ -259,29 +255,29 @@ if __name__ == '__main__':
     # # uuid = compressUuid("0ffbc6e5-f3dc-448f-9c98-607e124207fe")
     # # print(uuid)
 
-    # # 项目地址
-    # prog_path = ""
-    # # 需要替换文件的配置
-    # fileNameList = './tool_package/tiLang/i18nlist.txt';
-    # objNameList = []
-    # for i in open(fileNameList, 'r'):
-    #     objNameList.append(i.replace('\n', ''))
+    # 项目地址
+    prog_path = ""
+    # 需要替换文件的配置
+    fileNameList = './tool_package/tiLang/i18nlist.txt';
+    objNameList = []
+    for i in open(fileNameList, 'r'):
+        objNameList.append(i.replace('\n', ''))
 
-    # # 循环查找
-    # for i in objNameList:
-    #     iList = i.split(':')
-    #     if len(iList) < 3:
-    #         break
-    #     typeName = iList[0]
-    #     objName = r"".join(iList[1])
-    #     if os.path.isdir(objName):
-    #         # walkFile(objName)
-    #         ''
-    #     elif os.path.isfile(objName):
-    #         uuid = start_find_uuid(objName, i)
-    #         reuuid = compressUuid(uuid)
-    #         print(reuuid)
-    #         if iList[1]:
-    #             fileType = iList[2].split('-')
-    #         find_prefab(reuuid, fileType, typeName)
+    # 循环查找
+    for i in objNameList:
+        iList = i.split(':')
+        if len(iList) < 3:
+            break
+        typeName = iList[0]
+        objName = r"".join(iList[1])
+        if os.path.isdir(objName):
+            # walkFile(objName)
+            ''
+        elif os.path.isfile(objName):
+            uuid = start_find_uuid(objName, i)
+            reuuid = compressUuid(uuid)
+            print(reuuid)
+            if iList[1]:
+                fileType = iList[2].split('-')
+            find_prefab(reuuid, fileType, typeName)
 
