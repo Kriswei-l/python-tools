2aca228d049107866490e16ad03342a69188cba9
Kriswei-l <72719622+Kriswei-l@users.noreply.github.com>
2021-09-27 17:34:24 +0800

    Update GUIpy.py

--- learn_py/GUIpy.py
+++ learn_py/GUIpy.py
@@ -1,6 +1,6 @@
 '''
 Date: 2021-09-26 16:36:35
-LastEditTime: 2021-09-27 15:31:51
+LastEditTime: 2021-09-27 17:33:08
 FilePath: /python-tools/learn_py/GUIpy.py
 Description: 图形界面
 '''
@@ -19,29 +19,32 @@ class Application(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.fileUtil = FilsUtils()
+        self.labels = {}
         self.nameInput = {}
         self.alertButton={}
-        self.pack()
+        self.grid()
+        
         self.createWidgets()
 
     def createWidgets(self):
-        pathArr = self.fileUtil.readTxt("./learn_py/conf.txt")
+        pathArr = self.fileUtil.readTxtEx("./learn_py/conf.txt")
         for index,x in enumerate(pathArr):
             # print(index, x)
-            self.creatBtn(index,x)
+            self.creatBtn(index,x[0],x[1])
 
-    def creatBtn(self, index, path):
+    def creatBtn(self, index, labStr, path):
         # v=StringVar()
         # v.set("index")
-        self.nameInput[index] = Entry(self, width=70)
-        self.nameInput[index].pack()
+        self.labels[index] = Label(self, text=labStr, bg="Green", fg="white")
+        self.labels[index].grid(row=index, column = 0, padx = 10, pady=5, sticky=S+W)   #.pack(side='left')
+        self.nameInput[index] = Entry(self, width=76)
+        self.nameInput[index].grid(row=index, column = 1, padx = 10, pady=5)   #.pack(side='left')
         self.nameInput[index].insert(0,path)
-        # self.alertButton[index] = Button(self, text='发送')
-        # self.alertButton[index].bind(index, self.hello)
-        # self.alertButton[index].pack()
-        self.alertButton = Button(self, text='发送', command=lambda arg = index:self.hello(arg))
+        self.alertButton[index] = Button(self, text='发送', command=lambda arg = index:self.hello(arg))
+        self.alertButton[index].grid(row=index, column = 2, padx = 10, pady=5)   #.pack(side='left')
+        # self.alertButton = Button(self, text='发送', command=lambda arg = index:self.hello(arg))
         # self.alertButton.bind('<Return>', self.hello)
-        self.alertButton.pack()
+        # self.alertButton.pack()
 
     def hello(self, index):
         name = self.nameInput[index].get() or None
@@ -59,9 +62,22 @@ class Application(Frame):
         else:
             messagebox.showinfo('Message', '失败: %s' % url)
     
-
+# root = Tk()
+# canvas = Canvas(root,width=700,height=500,scrollregion=(0,0,700,3000))
+# canvas.place(x = 0, y = 0)
+# app = Application(canvas)
+# app.place(width=700,height=500)
+# # app.master.title("API接口")
+# app.master.minsize(700, 500)
+# scrollbar = Scrollbar(canvas, orient=VERTICAL)
+# # scrollbar.pack(side=RIGHT, fill=Y)
+# scrollbar.place(x=0,width=700,height=500)
+# scrollbar.configure(command=canvas.yview)
+# canvas.configure(yscrollcommand=scrollbar.set)
+# canvas.create_window((700, 500), window=app)
 
 app = Application()
 app.master.title("API接口")
 app.master.minsize(1000, 500)
+
 app.mainloop()
